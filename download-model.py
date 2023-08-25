dd2b46f35e29576cbda85b1b58bde471724828f9
Rafael Souza <rflpsz@gmail.com>
2023-08-25 18:11:05 -0300

    fixed download script

--- download-model.py
+++ download-model.py
@@ -3,7 +3,7 @@ import transformers
 model_name = "TheBloke/Llama-2-13B-Chat-fp16"
 
 # Download the model
-model = transformers.AutoModel.from_pretrained(model_name, output_dir="/models/13B/")
+model = transformers.AutoModel.from_pretrained(model_name)
 
 # Print the model name
 print(model.config.name)
