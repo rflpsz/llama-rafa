4dc684ad0da593d3fc45a3d6f8c07221a71bc315
Rafael Souza <rflpsz@gmail.com>
2023-08-25 17:40:26 -0300

    fixed context

--- predict.py
+++ predict.py
@@ -23,7 +23,7 @@ def main(
     )
 
     prompts = [
-        "I'm a vertical Fintech startup working on a SaaS model, I haven't made any sales yet and I'm in the development phase of my product, my financial planning is predicting an ARR of $800,000 dollars in the first year of operation, is it within the average or is there a different average for fintechs? If my revenue expectation is greater than the average, respond within a Json with the expectation and average value attributes, where the expectation must be = ABOVE AVERAGE If my revenue expectation is less than 30% of the average, respond with the expectation attribute = BELOW AVERAGE If my revenue expectation is greater than or equal to 30% or equal to the average, reply with the expectation attribute = WITHIN AVERAGE Answer only with json",
+        """I'm a vertical Fintech startup working on a SaaS model, I haven't made any sales yet and I'm in the development phase of my product, my financial planning is predicting an ARR of $800,000 dollars in the first year of operation, is it within the average or is there a different average for fintechs? If my revenue expectation is greater than the average, respond within a Json with the expectation and average value attributes, where the expectation must be = ABOVE AVERAGE If my revenue expectation is less than 30% of the average, respond with the expectation attribute = BELOW AVERAGE If my revenue expectation is greater than or equal to 30% or equal to the average, reply with the expectation attribute = WITHIN AVERAGE Answer only with json""",
     ]
     results = generator.text_completion(
         prompts,
