2dd1fb0d1e4248e6531e47d06a52f8aedc53e561
Rafael Souza <rflpsz@gmail.com>
2023-08-29 23:48:55 -0300

    fixed predict

--- gptq/predict.py
+++ gptq/predict.py
@@ -3,3 +3,10 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("TheBloke/Llama-2-13B-chat-GPTQ")
 model = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-13B-chat-GPTQ")
+
+prompt = "Tell me the first ten numbers of fibonaci sequence"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=30)
+tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
