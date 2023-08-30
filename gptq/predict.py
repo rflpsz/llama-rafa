8ee60751dc75486aad8c1ec1c8ceac89089d2dde
Rafael Souza <rflpsz@gmail.com>
2023-08-30 00:14:49 -0300

    fixed script

--- gptq/predict.py
+++ gptq/predict.py
@@ -1,18 +1,19 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM, QuantizationConfig
+from transformers import AutoModelForQuestionAnswering, QuantizationConfig
+
+# Create a model
+model = AutoModelForQuestionAnswering.from_pretrained("TheBloke/Llama-2-13B-chat-GPTQ")
 
 # Create a quantization config
 config = QuantizationConfig(disable_exllama=True)
 
-tokenizer = AutoTokenizer.from_pretrained("TheBloke/Llama-2-13B-chat-GPTQ")
-model = AutoModelForCausalLM.from_pretrained("TheBloke/Llama-2-13B-chat-GPTQ")
-
 # Quantize the model
 model = model.quantize(config)
 
-prompt = "Tell me the first ten numbers of fibonaci sequence"
-inputs = tokenizer(prompt, return_tensors="pt")
+# Create a question
+question = "What is the capital of France?"
 
-# Generate
-generate_ids = model.generate(inputs.input_ids, max_length=30)
-tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+# Get the answer
+answer = model.answer(question)
+
+# Print the answer
+print(answer)
